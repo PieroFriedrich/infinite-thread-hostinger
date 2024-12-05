00776fb40759ee4423ef7488de25f31576faa362
Piero Friedrich <pierofriedrich@yahoo.com.br>
2024-12-05 00:21:08 -0800

    fix link to post page

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -55,54 +55,49 @@ function Post({ post }) {
 
   return (
     <div className="my-8">
-      <Link to={`/post/${id}`}>
-        <div className="bg-myblue w-[90%] mx-auto p-0 rounded-tl-xl rounded-tr-xl">
-          <div className="relative p-3">
-            <div className="flex items-center gap-2">
-              <img
-                src={Logo}
-                alt="Infinite Thread Logo"
-                width={30}
-                height={30}
-              />
-              <p className="text-white">{author}</p>
-            </div>
-            <div className="bg-myorange text-white absolute top-0 right-0 p-1 rounded-tr-xl rounded-bl-xl">
-              Group Category
-            </div>
-            <h2 className="text-white text-3xl py-3 border-b border-b-myorange">
+      <div className="bg-myblue w-[90%] mx-auto p-0 rounded-tl-xl rounded-tr-xl">
+        <div className="relative p-3">
+          <div className="flex items-center gap-2">
+            <img src={Logo} alt="Infinite Thread Logo" width={30} height={30} />
+            <p className="text-white">{author}</p>
+          </div>
+          <div className="bg-myorange text-white absolute top-0 right-0 p-1 rounded-tr-xl rounded-bl-xl">
+            Group Category
+          </div>
+          <h2 className="text-white text-3xl py-3 border-b border-b-myorange">
+            <Link to={`/post/${id}`} className="text-white">
               {title}
-            </h2>
-            <p className="text-white pt-3 text-sm">{details}</p>
-          </div>
+            </Link>
+          </h2>
+          <p className="text-white pt-3 text-sm">{details}</p>
         </div>
-        <div className="bg-myorange w-[90%] mx-auto p-0 rounded-bl-xl rounded-br-xl flex gap-1 py-2 justify-between items-center">
-          <div className="flex ml-1 gap-2">
-            <LikeButton
-              postId={id}
-              isLiked={isLiked}
-              likeCount={likeCount}
-              onLikeChange={(newLikeCount, newIsLiked) => {
-                setLikeCount(newLikeCount);
-                setIsLiked(newIsLiked);
-              }}
-            />
-            <span className="text-white">{likeCount}</span>
-          </div>
+      </div>
+      <div className="bg-myorange w-[90%] mx-auto p-0 rounded-bl-xl rounded-br-xl flex gap-1 py-2 justify-between items-center">
+        <div className="flex ml-1 gap-2">
+          <LikeButton
+            postId={id}
+            isLiked={isLiked}
+            likeCount={likeCount}
+            onLikeChange={(newLikeCount, newIsLiked) => {
+              setLikeCount(newLikeCount);
+              setIsLiked(newIsLiked);
+            }}
+          />
+          <span className="text-white">{likeCount}</span>
+        </div>
 
-          <div className="flex mr-1 gap-1">
-            {tags &&
-              tags.split(",").map((tag) => (
-                <p
-                  key={tag}
-                  className="bg-myyellow rounded-lg ml-2 px-2 text-myblue"
-                >
-                  {tag}
-                </p>
-              ))}
-          </div>
+        <div className="flex mr-1 gap-1">
+          {tags &&
+            tags.split(",").map((tag) => (
+              <p
+                key={tag}
+                className="bg-myyellow rounded-lg ml-2 px-2 text-myblue"
+              >
+                {tag}
+              </p>
+            ))}
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
