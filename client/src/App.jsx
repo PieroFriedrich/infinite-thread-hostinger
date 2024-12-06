0c1db8eb5573073c9011ab909778f21e24b03fcb
Piero Friedrich <pierofriedrich@yahoo.com.br>
2024-12-05 23:34:35 -0800

    create login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NewPost from "./pages/NewPost";
 import PostPage from "./pages/PostPage";
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/newpost" element={<NewPost />} />
         <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
