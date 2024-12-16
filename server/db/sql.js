c11986c55c31e29c3cf8440655a2ecfb4067488f
Piero Friedrich <pierofriedrich@yahoo.com.br>
2024-12-16 15:51:19 -0800

    update sql.js

--- server/db/sql.js
+++ server/db/sql.js
@@ -11,7 +11,6 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 30000,
 });
 
 // Simplified initialization function with connection management
